29f4c56374f59e92367d8780fe8d595b7abd8320
Simon Awiti <awitisimon23@gmail.com>
2018-10-16 15:33:26 +0300

    [Chore #161213041] added data structures

---
+++ app/version1/products/models.py
@@ -0,0 +1,73 @@
+"""/app/version1/products/models.py"""
+from flask import jsonify
+
+class Products(object):
+    def __init__(self):
+        """ Initialize empty Product list"""
+        self.Products_list = []
+        self.notfound = None
+
+    def create_Product(self, description, quantity, min_quantity_in_store, price_per_roll):
+        """Create products"""
+        self.products = {}
+
+        self.productId = len(self.Products_list)
+        self.products['description'] = description
+        self.products['quantity'] = quantity
+        self.products['min_quantity_in_store'] = min_quantity_in_store
+        self.products['price_per_roll'] = price_per_roll
+        self.products['product_id'] = self.productId + 1
+        self.Products_list.append(self.products)
+        return jsonify({
+            "message": "Successful.",
+            "Product": self.Products_list}), 201
+
+    def get_products(self):
+        """ get all products """
+        return jsonify({
+            "message": "Successful.",
+            "Products": self.Products_list}), 200
+
+    def delete_product(self, product_id):
+        """ delete product """
+        for product in self.Products_list:
+            if product['product_id'] == product_id:
+                self.Products_list.remove(product)
+                return jsonify({
+                    "message": "Delete Successful.",
+                    "Products": self.Products_list}), 201
+            self.notfound = True
+        if self.notfound is True:
+            return jsonify({
+                "message": "No product with that id.",
+                "Product": self.Products_list}), 404
+
+    def update_product(self, product_id, description, quantity, min_quantity_in_store, price_per_roll):
+        """ update product """
+        for product in self.Products_list:
+            if product['product_id'] == product_id:
+                product['description'] = description
+                product['quantity'] = quantity
+                product['min_quantity_in_store'] = min_quantity_in_store
+                product['price_per_roll'] = price_per_roll
+                return jsonify({
+                    "message": "Update Successful.",
+                    "Product": self.Products_list}), 201
+            self.notfound = True
+        if self.notfound is True:
+            return jsonify({
+                "message": "No product with that id.",
+                "Product": self.Products_list}), 404
+
+    def get_product(self, product_id):
+        """ get single product """
+        for product in self.Products_list:
+            if product['product_id'] == product_id:
+                return jsonify({
+                    "message": "Successful.",
+                    "Product": product}), 200
+            self.notfound = True
+        if self.notfound is True:
+            return jsonify({
+                "message": "No product with that id.",
+                "Product": self.Products_list}), 404
