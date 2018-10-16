fbb521f909d34cb6e3cceac95774d127a96e74c6
Simon Awiti <awitisimon23@gmail.com>
2018-10-16 15:29:37 +0300

    [Chore #161213041] register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,9 @@ from flask import Flask
 def create_app():
     '''creating app'''
     app = Flask(__name__)
+
+    '''importing and registering the blueprints'''
+    from app.version1.products.views import version1_blueprints
+    app.register_blueprint(version1_blueprints)
+
     return app
