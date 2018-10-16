e408362b6744e0c155d24272a9eab3db316d2e6c
Simon Awiti <awitisimon23@gmail.com>
2018-10-16 15:56:04 +0300

    [feature #161213134] get a specific product and delete

--- app/version1/products/views.py
+++ app/version1/products/views.py
@@ -4,6 +4,7 @@ from models import Products
 
 ProductsObject = Products()
 
+
 version1_blueprints = Blueprint('version1', __name__, url_prefix='/api/v1/products')
 
 @version1_blueprints.route('/', methods=['GET', 'POST'])
@@ -19,3 +20,31 @@ def product():
         return response
     data = ProductsObject.get_products()
     return data
+
+@version1_blueprints.route('/<int:product_id>', methods=['GET', 'PUT', 'DELETE'])
+def product_manipulation(product_id, **kwargs):
+    """ GET/PUT/DEL product """
+    if request.method == 'DELETE':
+        '''DELETE'''
+        response = ProductsObject.delete_product(product_id)
+        return response
+
+    elif request.method == 'PUT':
+        '''PUT'''
+        data = request.get_json()
+        description = data['description']
+        quantity = data['quantity']
+        min_quantity_in_store = data['min_quantity_in_store']
+        price_per_roll = data['price_per_roll']
+        response = ProductsObject.update_product(
+            product_id,
+            description,
+            quantity,
+            min_quantity_in_store,
+            price_per_roll)
+        return response
+
+    else:
+        '''GET'''
+        response = ProductsObject.get_product(product_id)
+        return response
