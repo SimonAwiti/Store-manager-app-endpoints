d502e76433ed94f1c2813cffe57e4723ba3d1aed
Simon Awiti <awitisimon23@gmail.com>
2018-10-17 06:57:36 +0300

    [chore #161256253] updated requirements.txt

--- app/tests/test_products.py
+++ app/tests/test_products.py
@@ -2,13 +2,14 @@
 import unittest
 import json
 from app import create_app
+from app.version1.products.models import Products
 
 class TestBase(unittest.TestCase):
     """ Tests Base """
 
-    def create_app(self):
+    def create_app():
         """ Instantiate tests"""
-        app = create_app(self)
+        app = create_app()
         return app
 
 class TestProducts(TestBase):
@@ -23,14 +24,14 @@ class TestProducts(TestBase):
             price_per_roll="Ksh 400"))
         self.client = app.test_client()
         self.client.post(
-            '/v1/products',
+            '/api/v1/products/',
             data=self.create_products,
             content_type='application/json')
 
     def test_products_creation(self):
         """ Test for products creation """
         resource = self.client.post(
-            '/v1/products',
+            '/api/v1/products/',
             data=self.create_products,
             content_type='application/json')
 
@@ -42,7 +43,7 @@ class TestProducts(TestBase):
     def test_get_all_products(self):
         """ Test for getting all products """
         resource = self.client.get(
-            '/v1/products',
+            '/api/v1/products',
             data=json.dumps(dict()),
             content_type='application/json')
 
@@ -53,7 +54,7 @@ class TestProducts(TestBase):
 
     def test_get_product_by_product_id(self):
         """ Test for getting specific product """
-        resource = self.client.get('/v1/products/1')
+        resource = self.client.get('/api/v1/products/1')
         self.assertEqual(resource.status_code, 200)
 
 if __name__ == '__main__':
