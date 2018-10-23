cb5a8cb3f2d5ceb41157ec10d6fbc35305677c9a
Simon Awiti <awitisimon23@gmail.com>
2018-10-24 00:59:38 +0300

    [chore #161432832] create tables for the db

---
+++ app/version2/database/create_table.py
@@ -0,0 +1,29 @@
+users_table = """CREATE TABLE IF NOT EXISTS users
+            (
+                user_id serial PRIMARY KEY, 
+                username VARCHAR (50) UNIQUE NOT NULL, 
+                password VARCHAR (50) NOT NULL, 
+                email VARCHAR (355) UNIQUE NOT NULL,
+                role varchar(25) NOT NULL DEFAULT
+        )"""
+
+salesrec_table = """CREATE TABLE IF NOT EXISTS salesrecs
+            (
+	            salesrec_id  serial PRIMARY KEY,
+	            description varchar(25) UNIQUE NOT NULL,
+                datesold timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                unit_price varchar(25) UNIQUE NOT NULL,
+                quantity varchar(25) UNIQUE NOT NULL,
+                bill varchar(25) UNIQUE NOT NULL,
+                attendant varchar(25) UNIQUE NOT NULL
+        )"""
+products_table = """CREATE TABLE IF NOT EXISTS products
+            (
+	            product_id  serial PRIMARY KEY,
+	            description varchar(25) UNIQUE NOT NULL,
+                quantity varchar(25) UNIQUE NOT NULL,
+                price_per_unit varchar(25) UNIQUE NOT NULL,
+                total_cost varchar(25) UNIQUE NOT NULL
+        )"""
+
+queries = [users_table, salesrec_table, products_table]
